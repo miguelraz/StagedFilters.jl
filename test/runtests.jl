b275e9f9e349af28434d13ae218814c1fb3d6717
miguel raz <miguelraz@ciencias.unam.mx>
2019-04-25 17:19:17 -0500

    PUBLISH BABY

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,12 @@
 using StagedFilters
 using Test
 
-@testset "StagedFilters.jl" begin
-    # Write your own tests here.
+using PyCall, BenchmarkTools;
+data = convert.(Float64,collect(range(1,1000,length=1000))); smoothed = zeros(eltype(data),length(data)); # <--- wholesome, type stable code.
+savgol = pyimport("scipy.signal")."savgol_filter";
+x = PyObject(data);
+benchjl = @btime smooth!(SavitzkyGolayFilter{2,2}, data, smoothed);
+benchpy = @btime $savgol($x,5,2,mode="wrap");
+@testset "Correctness" begin
+    @test benchpy ≈ benchjl
 end
