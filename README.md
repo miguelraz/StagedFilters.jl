bce5902e00b3ebd4018b2cd28276e87befdd702b
miguel raz <miguelraz@gmail.com>
2020-11-14 17:17:29 -0600

    Julia 1.5.2 and new links. Cleaned up the runtests.jl file

--- README.md
+++ README.md
@@ -2,5 +2,14 @@
 
 This package implements a staged-programming approach for a highly specialized version of a Savitzky-Golay filter.
 
-If you would like to read a full blog post on the implementation on it, [you can find some here](https://miguelraz.github.io/2019/04/24/smoothingjuliajiahao.html).
+If you would like to read a full blog post on the implementation on it, [you can find some here](miguelraz.github.io/blog).
 For a video explanation by Stefan Karpinski, check out the [following video](https://www.youtube.com/watch?v=DRKKAFYM9yo&feature=youtu.be&t=2047)
+
+### Acknowledgments
+
+Many people contributed to the eventual development of this code:
+- Jiahao Chen, Matt Bauman and others for the original implementation
+- Stefan Karpinski for some branch elisions
+- Steven G Johnson, for help when benchmarking a PyCall code and being careful not to measure the conversion back and forth.
+
+If you know the measurement to be missing / not be as fair as it could be, please reach out and let me know - we're trying to compare best in class implementations side by side, not cheat out. In particular, I don't know if the `savgol` method I'm calling is in place or not, and would appreciate if somone could point out how to opt into that.
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,21 +1,33 @@
 using StagedFilters
 using Test
+using PyCall
+using BenchmarkTools;
 
-using PyCall, BenchmarkTools;
-data = convert.(Float64,collect(range(1,1000,length=1000))); 
-smoothed = zeros(eltype(data),length(data)); # <--- wholesome, type stable code.
-savgol = pyimport("scipy.signal")."savgol_filter";
-x = PyObject(data);
-benchjl64 = @btime smooth!(SavitzkyGolayFilter{2,2}, data, smoothed);
-benchpy64 = @btime $savgol($x,5,2,mode="wrap");
 
-data = convert.(Float32,collect(range(1,1000,length=1000))); 
-smoothed = zeros(eltype(data),length(data)); # <--- wholesome, type stable$
-savgol = pyimport("scipy.signal")."savgol_filter";
-x = PyObject(data);
-benchjl32 = @btime smooth!(SavitzkyGolayFilter{2,2}, data, smoothed);
-benchpy32 = @btime $savgol($x,5,2,mode="wrap");
-@testset "Correctness" begin
+@testset "StagedFilters.jl" begin
+    N = 1000
+    data = convert.(Float64, collect(range(1, N , length = N))); 
+    smoothed = zeros(eltype(data), length(data)); # <--- wholesome, type stable code.
+    savgol = pyimport("scipy.signal")."savgol_filter";
+    x = PyObject(data);
+
+    @info "Julia Float64"
+    benchjl64 = @btime smooth!(SavitzkyGolayFilter{2,2}, data, smoothed);
+
+    @info "Python Float64"
+    benchpy64 = @btime $savgol($x,5,2,mode="wrap");
+
+    data = convert.(Float32, collect(range(1, N, length = N))); 
+    smoothed = zeros(eltype(data), length(data)); # <--- wholesome, type stable$
+    #savgol = pyimport("scipy.signal")."savgol_filter";
+    x = PyObject(data);
+
+    @info "Julia Float64"
+    benchjl32 = @btime smooth!(SavitzkyGolayFilter{2,2}, data, smoothed);
+
+    @info "Python Float64"
+    benchpy32 = @btime $savgol($x,5,2,mode="wrap");
+
     @test benchpy64 ≈ benchjl64
     @test benchpy32 ≈ benchjl32
 end
